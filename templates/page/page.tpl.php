33b405061b5a20a1d712bc46cb8e7804bf198969
AlexBrown-appnovation <alex@appnovation.com>
2015-11-27 16:22:49 -0800

    some minor styling to default page elements

--- templates/page/page.tpl.php
+++ templates/page/page.tpl.php
@@ -6,6 +6,11 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728148
  */
+
+// Render the sidebars to see if there's anything in them.
+$sidebar_first  = render($page['sidebar_first']);
+$sidebar_second = render($page['sidebar_second']);
+
 ?>
 
 <div id="main" class="main-content" role="main">
@@ -16,7 +21,7 @@
       <div class="usa-grid">
         <div class="usa-banner-content" id="main-content">
 
-          <h1><?php print $site_name; ?></h1>
+          <a class="usa-banner-site-name" href="/"><h1><?php print $site_name; ?></h1></a>
 
           <?php if ($site_slogan): ?>
             <h2 class="usa-font-lead"><?php print $site_slogan; ?></h2>
@@ -46,6 +51,15 @@
 
   <section class="usa-section">
     <div class="usa-grid">
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <aside class="usa-width-one-third">
+          <?php print $sidebar_first; ?>
+          <?php print $sidebar_second; ?>
+        </aside>
+      <?php endif; ?>
+      <?php if ($sidebar_first || $sidebar_second): ?>
+        <div class="usa-width-two-thirds">
+      <?php endif; ?>
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print $messages; ?>
@@ -56,19 +70,7 @@
       <?php endif; ?>
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
-
-      <?php
-        // Render the sidebars to see if there's anything in them.
-        $sidebar_first  = render($page['sidebar_first']);
-        $sidebar_second = render($page['sidebar_second']);
-      ?>
-
-      <?php if ($sidebar_first || $sidebar_second): ?>
-        <aside class="usa-width-one-fourth">
-          <?php print $sidebar_first; ?>
-          <?php print $sidebar_second; ?>
-        </aside>
-      <?php endif; ?>
+      </div>
 
     </div>
   </section>
